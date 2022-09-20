ce5ba7948e5d3009a334531bc4f0ac1b3a24b4e7
stanislav-f1 <stanislav200z@mail.ru>
2022-09-20 18:16:22 +0300

    first function

--- 0-module/1-task/task.test.js
+++ 0-module/1-task/task.test.js
@@ -1,5 +1,11 @@
 describe('0-module-1-task', () => {
-  it('1+1=2', () => {
-    expect(sum(1, 1)).toEqual(2);
-  });
+    it('1+1=2', () => {
+        expect(sum(1, 1)).toEqual(2);
+    });
+});
+
+describe('0-module-1-task', () => {
+    it('2+5=7', () => {
+        expect(sum(2, 5)).toEqual(7);
+    });
 });
