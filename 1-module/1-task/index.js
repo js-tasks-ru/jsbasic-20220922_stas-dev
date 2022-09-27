461391e96845f08543443d10d26aedf425877866
stanislav-f1 <stanislav200z@mail.ru>
2022-09-27 13:17:14 +0300

    let

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,7 +1,7 @@
 function factorial(n) {
   if (n === 0 || n === 1)
       return 1;
-    for (var i = n - 1; i >= 1; i--) {
+    for (let i = n - 1; i >= 1; i--) {
       n = n * i; 
     }
     return n; 
