d9f2729cd4cc2c5a47dbf4d7ee459bf0c3f82391
stanislav-f1 <stanislav200z@mail.ru>
2022-10-03 16:34:17 +0300

    JavaScript Basics(3,4,5)

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,9 @@
-function truncate(str, maxlength) {
-  // ваш код...
+function truncate(str, maxlength = 20) {
+  
+  if(str.length > maxlength) {
+    return str.slice(0, maxlength - 1) + '…';
+  }
+
+  return str;
+
 }
