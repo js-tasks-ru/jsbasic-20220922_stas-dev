14dbd5e4c7f061e97991b33fe3d51e05e90ea635
stanislav-f1 <stanislav200z@mail.ru>
2022-10-10 18:33:49 +0300

    function namify, filterRange, camelize(str)

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+  return str
+      .split('-') 
+      .map(
+        function(word, index) {
+          if (index === 0) {
+            return word;
+          } else {
+            return word[0].toUpperCase() + word.slice(1);
+          }
+        }
+        )
+      .join('');
 }
