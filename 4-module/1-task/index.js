91576bec3bbda55c74acd7bc05e6dabe884f3592
stanislav-f1 <stanislav200z@mail.ru>
2022-10-20 18:52:12 +0300

    4-module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  
+  let list = document.createElement('UL');
+  list.innerHTML = friends.map(friend => `<li> ${friend.firstName} ${friend.lastName} </li>`)
+  .join('');
+
+  return list;
 }
