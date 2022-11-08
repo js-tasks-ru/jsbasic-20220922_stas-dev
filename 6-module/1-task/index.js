abfda3a5dce98b4b10dd652ecb7d96f9ac3d9c8e
stanislav-f1 <stanislav200z@mail.ru>
2022-11-08 13:25:01 +0300

    6 module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,52 @@
  *      }
  *
  */
-export default class UserTable {
+ export default class UserTable {
   constructor(rows) {
+      this.elem = this.render(rows);
+  }
+
+  render(rows) {
+    
+    let table = document.createElement('table');
+
+    let list = rows.reduce(function (result, row) {
+      return result + 
+      `
+      <tr>
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button>X</button></td>
+      </tr>
+    ` 
+    }, '');
+    
+    table.innerHTML = `
+      <table>
+        <thead>
+          <tr>
+            <td>Имя</td>
+            <td>Возраст</td>
+            <td>Зарплата</td>
+            <td>Город</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>${list}</tbody>
+      </table>
+    `
+    let btn = table.querySelectorAll('button');
+
+    for (let key of btn) {
+        key.addEventListener('click', this.onClick);
+    }
+
+    return table;
+  }
+
+  onClick(evt) {
+      evt.target.closest('tr').remove();
   }
 }
